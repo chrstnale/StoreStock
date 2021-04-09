21fe3286beae4a0c7520fbe3ee6519958cf0e91b
chrstnale <christianale85@gmail.com>
2021-04-10 00:56:10 +0700

    Added Product Controller and Model

---
+++ Controllers/ProductsController.cs
@@ -0,0 +1,24 @@
+﻿using StoreStock.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace StoreStock.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            List<ProductModel> products = new List<ProductModel>();
+
+            products.Add(new ProductModel("TEE01-BL", "T-Shirt Dragon", 100, 90, 8, 10, 100000));
+            products.Add(new ProductModel("TEE02-BL", "T-Shirt Unicorn", 100, 80, 8, 10, 100000));
+            products.Add(new ProductModel("TEE03-BL", "T-Shirt Phoenix", 100, 70, 8, 10, 100000));
+
+            return View("Index", products);
+        }
+    }
+}
---
+++ Models/ProductModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StoreStock.Models
+{
+    public class ProductModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public int TotalAmount { get; set; }
+        public int ReadyAmount { get; set; }
+        public int OrderedAmount { get; set; }
+        public int SoldAmount { get; set; }
+        public float Price { get; set; }
+
+        public ProductModel()
+        {
+            Code = "TEE01-BL";
+            Name = "Blue Dragon T-Shirt";
+            TotalAmount = 100;
+            ReadyAmount = 90;
+            OrderedAmount = 8;
+            SoldAmount = 10;
+            Price = 100000;
+        }
+
+        public ProductModel(string code, string name, int totalAmount, int readyAmount, int orderedAmount, int soldAmount, float price)
+        {
+            Code = code;
+            Name = name;
+            TotalAmount = totalAmount;
+            ReadyAmount = readyAmount;
+            OrderedAmount = orderedAmount;
+            SoldAmount = soldAmount;
+            Price = price;
+        }
+    }
+}
